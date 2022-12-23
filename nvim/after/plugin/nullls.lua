47e083060401c8296512a5a7e58649a547588a2d
Prashant <prashantacharya@lftechnology.com>
2022-12-23 17:51:50 +0545

    Config Prettier

--- nvim/after/plugin/nullls.lua
+++ nvim/after/plugin/nullls.lua
@@ -5,7 +5,7 @@ local event = "BufWritePre" -- or "BufWritePost"
 local async = event == "BufWritePost"
 
 null_ls.setup({
-  autocmd = function(client, bufnr)
+  on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.keymap.set("n", "<Leader>f", function()
         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
