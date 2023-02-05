902213c58c8893a25696143763f70e70bb51584d
Prashant <prashantacharya@lftechnology.com>
2023-02-05 15:25:32 +0545

    add remaps

--- nvim/lua/prashant/remap.lua
+++ nvim/lua/prashant/remap.lua
@@ -21,3 +21,13 @@ vim.keymap.set("x", "<M-k>", ":m '<-2<CR>gv=gv") -- move code block up
 -- switch buffer
 vim.keymap.set("n", "<M-l>", "<cmd>bnext<CR>")
 vim.keymap.set("n", "<M-h>", "<cmd>bprev<CR>")
+
+--  git signals
+vim.keymap.set("n", "gn", "<cmd>Gitsigns next_hunk<CR>")
+vim.keymap.set("n", "gN", "<cmd>Gitsigns prev_hunk<CR>")
+vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>")
+vim.keymap.set("n", "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<CR>")
+vim.keymap.set("n", "<leader>gd", "<cmd>Gitsigns toggle_deleted<CR>")
+vim.keymap.set("n", "<leader>gw", "<cmd>Gitsigns toggle_word_diff<CR>")
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<CR>")
+vim.keymap.set("n", "<leader>gt", "<cmd>Telescope git_branches<CR>")
