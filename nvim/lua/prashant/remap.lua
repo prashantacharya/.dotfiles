dd2b660f8a5a63c83a26962065ccf3389ffd0f57
Prashant <prashantacharya@lftechnology.com>
2023-02-05 15:35:50 +0545

    remove shortcuts to move line up and down

--- nvim/lua/prashant/remap.lua
+++ nvim/lua/prashant/remap.lua
@@ -13,14 +13,9 @@ vim.keymap.set("n", "K", ":move -2<CR>==")
 -- close current buffer on ctrl+w
 vim.keymap.set("n", "<C-w>", ":bdelete<CR>")
 
-vim.keymap.set("n", "<M-j>", ":m+<CR>") -- move line down
-vim.keymap.set("n", "<M-k>", ":m-2<CR>") -- move line up
-vim.keymap.set("x", "<M-j>", ":m '>+1<CR>gv=gv") -- move code block down
-vim.keymap.set("x", "<M-k>", ":m '<-2<CR>gv=gv") -- move code block up
-
 -- switch buffer
-vim.keymap.set("n", "<M-l>", "<cmd>bnext<CR>")
-vim.keymap.set("n", "<M-h>", "<cmd>bprev<CR>")
+vim.keymap.set("n", "<leader-h>", "<cmd>bnext<CR>")
+vim.keymap.set("n", "<leader-l>", "<cmd>bprev<CR>")
 
 --  git signals
 vim.keymap.set("n", "gn", "<cmd>Gitsigns next_hunk<CR>")
