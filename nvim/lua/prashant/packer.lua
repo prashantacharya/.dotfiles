f9f7b44ac5db87591373cd349866a4ecc7cdd40e
Prashant <prashantacharya@lftechnology.com>
2023-03-03 21:06:13 +0545

    Add which key

--- nvim/lua/prashant/packer.lua
+++ nvim/lua/prashant/packer.lua
@@ -110,4 +110,17 @@ return require("packer").startup(function(use)
 			}
 		end,
 	})
+	-- Lua
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 end)
