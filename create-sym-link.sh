097ec6a0bb6f5c72d87bd22bc9b4cf633eea80b3
Prashant <prashantacharya@lftechnology.com>
2023-03-05 22:24:35 +0545

    update sym link path

--- create-sym-link.sh
+++ create-sym-link.sh
@@ -2,7 +2,7 @@ echo "Creating symbolink for Neovim configuration"
 ln -s nvim ~/.config/nvim
 
 echo "Creating symbolink for Tmux configuration"
-ln -s .tmux.conf ~/.tmux.conf
+ln -s tmux ~/.config/tmux/tmux.conf
 
 echo "Creating symlink for .zshrc"
 ln -s .zshrc ~/.zshrc
