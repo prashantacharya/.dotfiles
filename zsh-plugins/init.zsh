c02354fbdd03e2fff187c53316a2b1942d53e703
Prashant <prashant.acharya@optible.ai>
2023-11-28 22:01:56 +0545

    Add aliases

---
+++ zsh-plugins/init.zsh
@@ -0,0 +1,5 @@
+CURRENT_DIR=$HOME/Projects/.dotfiles/zsh-plugins
+
+source "$CURRENT_DIR/git/git.plugin.zsh"
+source "$CURRENT_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$CURRENT_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
