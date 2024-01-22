6c5f65a385cd83a63ea384cede0a6a3e571b24f3
Prashant Acharya <dev.prashaant@gmail.com>
2024-01-23 04:20:42 +0545

    Add alacritty config and fix sym link

--- create-sym-link.sh
+++ create-sym-link.sh
@@ -1,8 +1,11 @@
-echo "Creating symbolink for Neovim configuration"
-ln -s nvim ~/.config/nvim
+echo "Creating symlink for Neovim configuration"
+ln -s ~/Projects/.dotfiles/nvim ~/.config/nvim
 
-echo "Creating symbolink for Tmux configuration"
-ln -s tmux ~/.config/tmux/tmux.conf
+echo "Creating symlink for Tmux configuration"
+ln -s ~/Projects/.dotfiles/tmux ~/.config/tmux
 
 echo "Creating symlink for .zshrc"
-ln -s .zshrc ~/.zshrc
+ln -s ~/Projects/.dotfiles/.zshrc ~/.zshrc
+
+echo "Creating symlink for alacritty"
+ln -s ~/Projects/.dotfiles/alacritty ~/.config/alacritty
