8fe5f1f60886b512429dd8c3255c58856c744b7a
Prashant <dev.prashaant@gmail.com>
2026-07-22 00:05:28 -0400

    Seemlessly switch between nvim and wezterm panes

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,57 +1,81 @@
 return {
-	-- Core / LSP (coc)
-	{
-		"neoclide/coc.nvim",
-		branch = "release",
-		lazy = false,
-		build = "npm install --frozen-lockfile || npm install",
-	},
+  -- Core / LSP (coc)
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    lazy = false,
+    build = "npm install --frozen-lockfile || npm install",
+  },
 
-	-- Colorscheme (after/plugin/colors.lua)
-	{ "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+  -- Colorscheme (after/plugin/colors.lua)
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
 
-	-- Treesitter stack (after/plugin/treesitter*.lua, autotag.lua)
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-context",
-			"windwp/nvim-ts-autotag",
-		},
-	},
-	{ "nvim-treesitter/nvim-treesitter-context", lazy = false },
-	{ "windwp/nvim-ts-autotag", lazy = false },
+  -- Treesitter stack (after/plugin/treesitter*.lua, autotag.lua)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-context",
+      "windwp/nvim-ts-autotag",
+    },
+  },
+  { "nvim-treesitter/nvim-treesitter-context", lazy = false },
+  { "windwp/nvim-ts-autotag", lazy = false },
 
-	-- Editor UI
-	{
-		"goolord/alpha-nvim",
-		lazy = false,
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{ "nvim-tree/nvim-tree.lua", lazy = false },
-	{ "nvim-lualine/lualine.nvim", lazy = false },
-	{ "feline-nvim/feline.nvim", lazy = false },
-	{ "lukas-reineke/indent-blankline.nvim", lazy = false },
+  -- Editor UI
+  {
+    "goolord/alpha-nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  { "nvim-tree/nvim-tree.lua", lazy = false },
+  { "nvim-lualine/lualine.nvim", lazy = false },
+  { "feline-nvim/feline.nvim", lazy = false },
+  { "lukas-reineke/indent-blankline.nvim", lazy = false },
 
-	-- Editing
-	{ "windwp/nvim-autopairs", lazy = false },
-	{ "terrortylor/nvim-comment", lazy = false },
+  -- Editing
+  { "windwp/nvim-autopairs", lazy = false },
+  { "terrortylor/nvim-comment", lazy = false },
 
-	-- Git
-	{ "lewis6991/gitsigns.nvim", lazy = false },
-	{ "sindrets/diffview.nvim", lazy = false, dependencies = { "nvim-lua/plenary.nvim" } },
+  -- Git
+  { "lewis6991/gitsigns.nvim", lazy = false },
+  { "sindrets/diffview.nvim", lazy = false, dependencies = { "nvim-lua/plenary.nvim" } },
 
-	-- Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		lazy = false,
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
-	-- AI completion
-	{ "supermaven-inc/supermaven-nvim", lazy = false },
+  -- AI completion
+  { "supermaven-inc/supermaven-nvim", lazy = false },
 
-	-- Utilities
-	{ "mbbill/undotree", lazy = false },
+  -- Utilities
+  { "mbbill/undotree", lazy = false },
+
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    config = function()
+      local smart_splits = require("smart-splits")
+
+      smart_splits.setup({
+        multiplexer_integration = "wezterm",
+      })
+
+      -- Navigate
+      vim.keymap.set("n", "<C-h>", smart_splits.move_cursor_left)
+      vim.keymap.set("n", "<C-j>", smart_splits.move_cursor_down)
+      vim.keymap.set("n", "<C-k>", smart_splits.move_cursor_up)
+      vim.keymap.set("n", "<C-l>", smart_splits.move_cursor_right)
+
+      -- Resize
+      vim.keymap.set("n", "<A-h>", smart_splits.resize_left)
+      vim.keymap.set("n", "<A-j>", smart_splits.resize_down)
+      vim.keymap.set("n", "<A-k>", smart_splits.resize_up)
+      vim.keymap.set("n", "<A-l>", smart_splits.resize_right)
+    end,
+  }
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,6 +7,7 @@ config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 18
 config.color_scheme = "Tokyo Night"
 config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
 
 -- Config for tab bar
 config.enable_tab_bar = true
@@ -14,13 +15,59 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"
 
+-- Move between panes and resize
+-- seemlessly with smart-splits.nvim
+local function is_nvim(pane)
+  return pane:get_user_vars().IS_NVIM == "true"
+end
+
+
+local directions = {
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+local function split_nav(mode, key)
+  return {
+    key = key,
+    mods = mode == "resize" and "ALT" or "CTRL",
+
+    action = wezterm.action_callback(function(window, pane)
+      if is_nvim(pane) then
+        -- Let smart-splits.nvim handle the key.
+        window:perform_action(
+          wezterm.action.SendKey {
+            key = key,
+            mods = mode == "resize" and "ALT" or "CTRL",
+          },
+          pane
+        )
+      elseif mode == "resize" then
+        window:perform_action(
+          wezterm.action.AdjustPaneSize {
+            directions[key],
+            3,
+          },
+          pane
+        )
+      else
+        window:perform_action(
+          wezterm.action.ActivatePaneDirection(directions[key]),
+          pane
+        )
+      end
+    end),
+  }
+end
 
 -- Config to split panes
 config.keys = {
   -- Split into left/right panes
   {
     key = "|",
-    mods = "CMD|SHIFT",
+    mods = "CTRL|SHIFT",
     action = wezterm.action.SplitHorizontal {
       domain = "CurrentPaneDomain",
     },
@@ -29,54 +76,23 @@ config.keys = {
   -- Split into top/bottom panes
   {
     key = "_",
-    mods = "CMD",
+    mods = "CTRL|SHIFT",
     action = wezterm.action.SplitVertical {
       domain = "CurrentPaneDomain",
     },
   },
 
+  -- Move between panes
+  split_nav("move", "h"),
+  split_nav("move", "j"),
+  split_nav("move", "k"),
+  split_nav("move", "l"),
 
-  {
-    key = "h",
-    mods = "CMD",
-    action = wezterm.action.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "j",
-    mods = "CMD",
-    action = wezterm.action.ActivatePaneDirection("Down"),
-  },
-  {
-    key = "k",
-    mods = "CMD",
-    action = wezterm.action.ActivatePaneDirection("Up"),
-  },
-  {
-    key = "l",
-    mods = "CMD",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-
-  {
-    key = "h",
-    mods = "ALT",
-    action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-  },
-  {
-    key = "j",
-    mods = "ALT",
-    action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-  },
-  {
-    key = "k",
-    mods = "ALT",
-    action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-  },
-  {
-    key = "l",
-    mods = "ALT",
-    action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-  },
+  -- Seamless Neovim/WezTerm resizing
+  split_nav("resize", "h"),
+  split_nav("resize", "j"),
+  split_nav("resize", "k"),
+  split_nav("resize", "l"),
 }
 
 return config
